faa35a69713aebe2c8e8ec772b65dd3cc4ae47a8
tooler <dengpengp@gmail.com>
2020-04-20 23:06:30 +0800

    完成功能左滑右滑 -------------------- 使用到转换器，其次是要把Xamarin.Forms包 升级到4.5,不然点没有效果

--- AppFiast/AppFiast.Android/MainActivity.cs
+++ AppFiast/AppFiast.Android/MainActivity.cs
@@ -19,6 +19,7 @@ namespace AppFiast.Droid
 
             base.OnCreate(savedInstanceState);
 
+            global::Xamarin.Forms.Forms.SetFlags("SwipeView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
